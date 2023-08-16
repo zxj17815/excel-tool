a3952378c649cbf5821d1bb9723a118ab5c32c46
iceiceice <597952291@qq.com>
2023-08-16 17:04:09 +0800

    update

--- src/main/customIpc.ts
+++ src/main/customIpc.ts
@@ -1,9 +1,12 @@
 import { dialog, ipcMain } from 'electron'
 import sqlIpc from './sql'
+import * as ExcelJS from 'exceljs'
 
 const customIpc = (): void => {
   //数据库查询
   sqlIpc()
+
+  //导入excel
   ipcMain.handle('upload-file', async (event) => {
     try {
       console.log('upload-file', event)
@@ -14,6 +17,17 @@ const customIpc = (): void => {
       if (!filePath) {
         return // 用户取消了选择
       } else {
+        const workbook = new ExcelJS.Workbook()
+        await workbook.xlsx.readFile(filePath)
+        // console.log(workbook)
+        workbook.eachSheet((worksheet, sheetId) => {
+          console.log('sheetId', sheetId)
+          console.log('worksheet', worksheet.name)
+        })
+        const worksheet = workbook.getWorksheet(2)
+        worksheet.eachRow((row, rowNumber) => {
+          console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values))
+        })
         return filePath
       }
     } catch (err) {
